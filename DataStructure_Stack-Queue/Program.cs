13a2107ff3d4ba7da38ea2d9cbbe2df52ff6a6b4
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-07 10:53:05 +0530

    Wellcome

---
+++ DataStructure_Stack-Queue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructure_Stack_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Data Structure Program");
+        }
+    }
+}
