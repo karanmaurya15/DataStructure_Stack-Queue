4c65a686939a7b325c4fd69f5163f8549b91974e
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-07 11:06:30 +0530

    Stack is created

--- DataStructure_Stack-Queue/Program.cs
+++ DataStructure_Stack-Queue/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to Data Structure Program");
+            Console.WriteLine("Wellcome to Stacks and Queues");
+            LinkedList stack = new LinkedList();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("\n--------stack elements--------");
+            stack.Display();
         }
     }
 }
